e23d050ba9a2b5818d054de04ba4e44898c13543
DmShko <vombat_schev@i.ua>
2023-07-28 19:54:40 +0300

    goit-js-hw-09-07

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,49 +1,64 @@
 import debounce from 'lodash';
 import Notiflix from 'notiflix';
 
+// most elements
 const elementSet = {
   fatherElement: document.querySelector('.form'),
   imputDelayElement: document.querySelector('[name="delay"]'),
-  imputDelayElement: document.querySelector('[name="step"]'),
-  imputDelayElement: document.querySelector('[name="amount"]'),
-  imputDelayElement: document.querySelector("button"),
+  imputStepElement: document.querySelector('[name="step"]'),
+  imputAmountElement: document.querySelector('[name="amount"]'),
+  imputSubmitElement: document.querySelector("button"),
 };
 
+// disable button
+elementSet.imputSubmitElement.disabled = true;
+
+// event handler
 const inputEvent = function(evt) {
 
   evt.preventDefault();
 
+  // gets fields values
   const { elements: { step , delay, amount } } = elementSet.fatherElement;
 
+  // if fields is not empty then button disable is 'false'
+  if(step.value !== "" && delay.value !== "" && amount.value !== "") {
+    
+    elementSet.imputSubmitElement.disabled = false;
+  }
+
+  // if button is pressed
   if(evt.target.getAttribute("type") === 'submit') {
     
-    if(step !== "" || delay !== "" || amount !== "") {
-      for(let i=0; i < currentAmount; i += 1) {
-        currentDelay += currentStep;
-        createPromise(currentDelay, i + 1)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay} ms`, {
-            timeout: 10000,
-          },);
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.warning(`Rejected  promise ${position} in ${delay} ms`,
-          {
-            timeout: 10000,
-          },);
-        });
-      }
+    for(let i=0; i < currentAmount; i += 1) {
+      currentDelay += currentStep;
+      //promises enable
+      createPromise(currentDelay, i + 1)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay} ms`, {
+          timeout: 10000,
+        },);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(`Rejected  promise ${position} in ${delay} ms`,
+        {
+          timeout: 10000,
+        },);
+      });
     }
-  } 
+  }
 
+  // input 'delay' event
   if(evt.target.getAttribute("name") === 'delay') {
     currentDelay = Number(delay.value);
   }
 
+  // input 'step' event
   if(evt.target.getAttribute("name") === 'step'){
     currentStep = Number(step.value);
   }
 
+  // input 'amount' event
   if(evt.target.getAttribute("name") === 'amount'){
     currentAmount = Number(amount.value);
   }
@@ -68,6 +83,5 @@ function createPromise(delay, position) {
         }
       }, delay)
       
-    });
-  
+    }); 
 }
